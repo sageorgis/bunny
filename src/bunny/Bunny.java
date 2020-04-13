27aea3d043e379644c201ffc1dc5fd87c2303b3a
sageorge <sageorgisnick@gmail.com>
2020-04-14 01:23:49 +0300

    test1

--- src/bunny/Bunny.java
+++ src/bunny/Bunny.java
@@ -11,7 +11,7 @@ package bunny;
  */
 public class Bunny {
     private int bunnyNumber;
-    
+    //tha grapsw kapoia sxoleia gia na to kanw commit kai push
     public Bunny(int bunnyNumber){
         this.bunnyNumber=bunnyNumber;
     }
